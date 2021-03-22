24809570b11dedde31adb176772351310bf2893b
amer <amer.issa@cloudified.ca>
2021-03-21 22:14:45 -0400

    fix python

--- installtableau.ps1
+++ installtableau.ps1
@@ -1,4 +1,5 @@
 
 ./python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Users\cibcadmin\AppData\Local\Programs\Python\Python37-32")
 
-python SilentInstaller.py install --secretsFile secrets.json --configFile myconfig.json --registrationFile registration.json Setup-Tabadmin-Webapp-x64.exe
+python SilentInstaller.py install --secretsFile secrets.json --configFile myconfig.json --registrationFile registration.json TableauServer-64bit-2020-4-2.exe
